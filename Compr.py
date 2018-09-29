22cf67e3a1054d5fcd6b0a2c9eedcb21da9d949a
Allan419 <42607056+Allan419@users.noreply.github.com>
2018-09-29 18:15:08 +0200

    Revert "added sth"
    This reverts commit 0a5d31955389d1da8f0209a1f628ede6ec706997.

--- Compr.py
+++
@@ -1,5 +0,0 @@
-names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
-heros = ['Batman','Superman','Spiderman','Wolverine','Deadpool']
-
-my_dict =[{name:hero} for name in names for hero in heros]
-print(my_dict)
